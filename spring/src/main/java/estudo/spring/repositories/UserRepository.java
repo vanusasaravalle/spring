b25799a6d54de0480f31c3eba70b80d8fc821741
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-08-25 09:58:11 -0300

    Update

--- spring/src/main/java/estudo/spring/repositories/UserRepository.java
+++ spring/src/main/java/estudo/spring/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import estudo.spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    User getOne(User id);
 }
--- spring/src/main/java/estudo/spring/services/UserService.java
+++ spring/src/main/java/estudo/spring/services/UserService.java
@@ -12,22 +12,55 @@ public class UserService {
 
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
 
         return userRepository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
 
         return obj.get();
     }
 
-     public User insertUser(User obj){
+    public User insertUser(User obj) {
         return userRepository.save(obj);
-     }
+    }
 
-     public void delete(Long id){
+    public void delete(Long id) {
         userRepository.deleteById(id);
-     }
+    }
+
+    public User update(User id, User user) {
+        User entity = userRepository.getOne(id);
+        update(entity, user);
+        return userRepository.save(entity);
+
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
